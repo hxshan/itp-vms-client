a9c81b290f2028d3005e9a5840055773fce811b6
Chamod Perera <chamod914@gmail.com>
2024-04-14 22:15:09 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import HireRequestForm from "./components/HireRequestForm";
 
 function App() {
   return (
     <Router>
         <Navbar /> 
         <Footer /> 
+
+        <Routes>
+          <Route path="/requesthire" element={<HireRequestForm />}/>
+        </Routes>
     </Router>
   );
 }
