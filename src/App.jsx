670789812ea96dea6753093f600d3ae82f38c9b1
wajeenpc <shaheedwajee@gmail.com>
2024-05-13 12:00:23 +0530

    contract detail view frontend made

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import LandingPage from './pages/LandingPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HireRequestForm from "./components/HireRequestForm";
+import ClientDashboard from "./pages/ClientDash";
+
 
 function App() {
   return (
     <Router>
         <Navbar /> 
-        
+
 
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/requesthire" element={<HireRequestForm />}/>
+          <Route path="/clientDash" element={<ClientDashboard/>} />
         </Routes>
 
-        <Footer /> 
+        
     </Router>
   );
 }
